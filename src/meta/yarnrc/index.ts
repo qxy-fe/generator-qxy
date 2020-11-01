7efd66a8122a1140cf9b62938ac4330b3cad877e
ntnyq <ntnyq13@gmail.com>
2020-11-02 03:45:15 +0800

    fix(yarnrc): add generator

---
+++ src/meta/yarnrc/index.ts
@@ -0,0 +1,28 @@
+import fs from 'fs'
+import ejs from 'ejs'
+import BaseGenerator from '../../base-generator'
+import type { GeneratorOptions } from 'yeoman-generator'
+
+export = class YarnrcGenerator extends BaseGenerator {
+  protected registry: string | false
+
+  constructor(args: string | string[], options: GeneratorOptions) {
+    super(args, options)
+
+    this.option('registry', {
+      type: String,
+      default: '',
+      description: 'registry to use',
+    })
+  }
+
+  async initializing(): Promise<void> {
+    this.registry = this.options.registry
+
+    const yarnrc = await ejs.renderFile(this.templatePath('.yarnrc.ejs'), {
+      registry: this.registry,
+    })
+
+    fs.writeFileSync(this.destinationPath('.yarnrc'), yarnrc)
+  }
+}

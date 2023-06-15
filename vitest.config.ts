4d8f53cd5a8cea20b79884ae41c2691849353e57
ntnyq <ntnyq13@gmail.com>
2023-06-15 15:40:31 +0800

    chore: bump deps

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,8 +2,7 @@ import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   test: {
-    // required for test generators
-    threads: false,
+    threads: false, // required for test generators
     testTimeout: 30_000,
     hookTimeout: 30_000,
     coverage: {

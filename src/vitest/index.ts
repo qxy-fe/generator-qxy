677867b61cb994c4c343929c70c328f09d405074
ntnyq <ntnyq13@gmail.com>
2023-09-14 10:56:33 +0800

    fix(vitest): use v8 coverage

--- src/vitest/index.ts
+++ src/vitest/index.ts
@@ -2,7 +2,7 @@ import BaseGenerator from '../base-generator'
 
 export default class VitestGenerator extends BaseGenerator {
   writing() {
-    const devDeps = ['vitest', '@vitest/coverage-c8']
+    const devDeps = ['vitest', '@vitest/coverage-v8']
 
     this.fs.copy(this.templatePath('vitest.config.ts'), this.destinationPath('vitest.config.ts'))
 

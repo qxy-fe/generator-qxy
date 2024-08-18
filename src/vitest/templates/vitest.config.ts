2fe9dbd44c8cf4a47e10a78e8f88327992c9c614
ntnyq <ntnyq13@gmail.com>
2024-08-18 14:21:25 +0800

    fix(vitest): tweak test config

--- src/vitest/templates/vitest.config.ts
+++ src/vitest/templates/vitest.config.ts
@@ -2,8 +2,9 @@ import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   test: {
+    reporters: 'dot',
     coverage: {
-      reporter: ['lcov', 'html', 'json', 'text'],
+      reporter: ['lcov', 'clover', 'text'],
     },
   },
 })

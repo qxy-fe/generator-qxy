114ce8a750503cfdb34af8d833efdf070aee3389
ntnyq <ntnyq13@gmail.com>
2024-12-23 22:38:44 +0800

    test: migrate to codecov

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,14 +3,14 @@ import { defineConfig } from 'vitest/config'
 export default defineConfig({
   test: {
     hookTimeout: 30_000,
+    // required for test generators
+    pool: 'forks',
+    reporters: ['dot'],
+    testTimeout: 30_000,
     coverage: {
       exclude: ['**/templates/**'],
       include: ['**/generators/**'],
       reporter: ['lcov', 'text'],
     },
-    // required for test generators
-    pool: 'forks',
-    reporters: ['dot'],
-    testTimeout: 30_000,
   },
 })

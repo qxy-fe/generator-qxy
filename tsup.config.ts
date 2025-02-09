adf5786d03bd5766170208d93e7c725c2247725c
ntnyq <ntnyq13@gmail.com>
2025-02-09 18:58:42 +0800

    chore: update

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,14 +3,11 @@ import { defineConfig } from 'tsup'
 export default defineConfig({
   clean: true,
   entry: ['src/**/*.ts', '!**/templates/*.ts'],
-  format: ['esm'],
+  format: 'esm',
   ignoreWatch: ['src/**/templates/*'],
   onSuccess: 'npm run copy',
   outDir: 'generators',
   shims: true,
   target: ['es2022', 'node18'],
   treeshake: 'safest',
-  esbuildOptions: options => {
-    options.charset = 'utf8'
-  },
 })

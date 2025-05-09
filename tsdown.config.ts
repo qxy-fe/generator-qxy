003862fddbb5441e82dbb9176ffee328db90a728
ntnyq <ntnyq13@gmail.com>
2025-05-09 09:28:19 +0800

    build: migrate to tsdown

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,13 +1,10 @@
-import { defineConfig } from 'tsup'
+import { defineConfig } from 'tsdown'
 
 export default defineConfig({
   clean: true,
   entry: ['src/**/*.ts', '!**/templates/*.ts'],
-  format: 'esm',
-  ignoreWatch: ['src/**/templates/*'],
   onSuccess: 'npm run copy',
   outDir: 'generators',
   shims: true,
   target: ['es2022', 'node18'],
-  treeshake: 'safest',
 })

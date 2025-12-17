86378c727780a9d9f53d28cc48fe87c5cdbf568d
ntnyq <ntnyq13@gmail.com>
2025-12-17 19:21:06 +0800

    build: set platform to node

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -3,10 +3,11 @@ import { defineConfig } from 'tsdown'
 export default defineConfig({
   clean: true,
   entry: ['src/**/*.ts', '!**/templates/*.ts'],
+  fixedExtension: false,
   onSuccess: 'npm run copy',
   outDir: 'generators',
   // use `.js` extension
-  platform: 'browser',
+  platform: 'node',
   shims: true,
   target: ['es2023', 'node18'],
 })
